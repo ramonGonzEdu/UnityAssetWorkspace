71f971a7fa898e6ee0d5a0cdfbc34b3351859731
Ramon Gonzalez Fernandez <recdeclec.ceo@gmail.com>
2021-10-09 19:04:26 -0400

    Tweak Minecraft v1

--- Assets/ExternalAssets/DaMastaCoda/Minecraft/v1/Scripts/CameraController.cs
+++ Assets/ExternalAssets/DaMastaCoda/Minecraft/v1/Scripts/CameraController.cs
@@ -1,78 +1,82 @@
 using System;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+
+namespace DaMastaCoda.Minecraft.v1
 {
-	[SerializeField] private float mouseSensitivity;
-	[SerializeField] private Transform playerBody;
-	[SerializeField] private Transform playerModel;
-	[SerializeField] private Transform playerHead;
-
-	[SerializeField] private float headRotationLimit;
-
-	private void Start()
+	public class CameraController : MonoBehaviour
 	{
-		Cursor.lockState = CursorLockMode.Locked;
-	}
+		[SerializeField] private float mouseSensitivity;
+		[SerializeField] private Transform playerBody;
+		[SerializeField] private Transform playerModel;
+		[SerializeField] private Transform playerHead;
 
-	private void Update()
-	{
-		Rotate();
-	}
+		[SerializeField] private float headRotationLimit;
 
-	private void Rotate()
-	{
-
-		float moveZ = Input.GetAxis("Vertical");
-
-		if (moveZ < 0)
+		private void Start()
 		{
-			float dxm = playerModel.localRotation.eulerAngles.y;
-			if (dxm > 180)
-				dxm = dxm - 360;
-			dxm = dxm * .01f;
-			playerModel.Rotate(Vector3.up, dxm);
-		}
-		if (moveZ > 0)
-		{
-			float dxm = playerModel.localRotation.eulerAngles.y;
-			if (dxm > 180)
-				dxm = dxm - 360;
-			dxm = dxm * -.008f;
-			playerModel.Rotate(Vector3.up, dxm);
+			Cursor.lockState = CursorLockMode.Locked;
 		}
 
-
-
-		float hrot = playerHead.localRotation.eulerAngles.x;
-		if (hrot > 180) hrot -= 360;
-
-		if (hrot > 60 || hrot < -60)
+		private void Update()
 		{
-			playerHead.Rotate(Vector3.right, (60 - Math.Abs(hrot)) * Math.Sign(hrot));
+			Rotate();
 		}
 
-		float xRotation = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-		float yRotation = -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-
-		playerBody.Rotate(Vector3.up, xRotation);
-		playerModel.Rotate(Vector3.up, -xRotation);
-		playerHead.Rotate(Vector3.right, yRotation);
-
-		float m = playerModel.localRotation.eulerAngles.y;
-		if (m > 180) m -= 360;
-
-		if (Mathf.Abs(m) > headRotationLimit)
+		private void Rotate()
 		{
-			float y = m;
-			float ys = Mathf.Sign(y);
-			float dy = (headRotationLimit - y * ys) * ys;
-			playerModel.Rotate(Vector3.up, dy);
 
+			float moveZ = Input.GetAxis("Vertical");
+
+			if (moveZ < 0)
+			{
+				float dxm = playerModel.localRotation.eulerAngles.y;
+				if (dxm > 180)
+					dxm = dxm - 360;
+				dxm = dxm * .01f;
+				playerModel.Rotate(Vector3.up, dxm);
+			}
+			if (moveZ > 0)
+			{
+				float dxm = playerModel.localRotation.eulerAngles.y;
+				if (dxm > 180)
+					dxm = dxm - 360;
+				dxm = dxm * -.008f;
+				playerModel.Rotate(Vector3.up, dxm);
+			}
+
+
+
+			float hrot = playerHead.localRotation.eulerAngles.x;
+			if (hrot > 180) hrot -= 360;
+
+			if (hrot > 60 || hrot < -60)
+			{
+				playerHead.Rotate(Vector3.right, (60 - Math.Abs(hrot)) * Math.Sign(hrot));
+			}
+
+			float xRotation = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+			float yRotation = -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+			playerBody.Rotate(Vector3.up, xRotation);
+			playerModel.Rotate(Vector3.up, -xRotation);
+			playerHead.Rotate(Vector3.right, yRotation);
+
+			float m = playerModel.localRotation.eulerAngles.y;
+			if (m > 180) m -= 360;
+
+			if (Mathf.Abs(m) > headRotationLimit)
+			{
+				float y = m;
+				float ys = Mathf.Sign(y);
+				float dy = (headRotationLimit - y * ys) * ys;
+				playerModel.Rotate(Vector3.up, dy);
+
+			}
+
+			playerHead.Rotate(Vector3.forward, -playerHead.localRotation.eulerAngles.z);
+			playerHead.Rotate(Vector3.up, -playerModel.localRotation.eulerAngles.y - playerHead.localRotation.eulerAngles.y);
 		}
-
-		playerHead.Rotate(Vector3.forward, -playerHead.localRotation.eulerAngles.z);
-		playerHead.Rotate(Vector3.up, -playerModel.localRotation.eulerAngles.y - playerHead.localRotation.eulerAngles.y);
 	}
 }
